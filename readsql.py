c57b1068494cf64e093bccf39fa2bf787de22987
Manish <manishjoshi9210@gmail.com>
2025-01-23 16:34:35 +0530

    second commit

---
+++ readsql.py
@@ -0,0 +1,43 @@
+import os
+
+import pandas as pd
+import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
+host = os.getenv('HOST')
+username = os.getenv('USER')
+password = os.getenv('PASSWORD')
+
+connection = mysql.connector.connect(host=host,
+                                     user=username,
+                                     password=password,
+                                     database='swiftmarket')
+
+cursor = connection.cursor()
+
+def queryToDataFrame(query):
+
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    df = pd.DataFrame(data=rows,columns=cursor.column_names)
+    return df
+
+def showTables():
+    query = """SHOW TABLES;"""
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    df = pd.DataFrame(data=rows,columns=cursor.column_names)
+    return df
+
+
+def DescribeTable(tablename):
+    query = f"""DESCRIBE {tablename};"""
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    df = pd.DataFrame(data=rows,columns=cursor.column_names)
+    return df
+
+def add(x,y):
+    return x+y
+
